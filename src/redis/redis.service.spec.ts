8541463c38d7fcbf86ff92faba00cda159efa0fe
PavlenkoDm <dmpavlenko07@gmail.com>
2025-06-24 20:42:39 +0300

    add unit tests for RedisService

--- src/redis/redis.service.spec.ts
+++ src/redis/redis.service.spec.ts
@@ -1,13 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { RedisService } from './redis.service';
+import { RedisClientName } from './redis-constants/redis.constants';
+import { SetUserDto } from './redis-dto/set-user.dto';
 
 describe('RedisService', () => {
   let service: RedisService;
+  const redisClientMock = {
+    set: jest.fn(),
+    get: jest.fn(),
+    del: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RedisService],
+      providers: [
+        RedisService,
+        { provide: RedisClientName.RADIS_CACHE, useValue: redisClientMock },
+      ],
     }).compile();
 
     service = module.get<RedisService>(RedisService);
@@ -15,5 +25,49 @@ describe('RedisService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('setUser', () => {
+    it('should call redisClient.set with correct arguments', async () => {
+      const key = 'user:1';
+      const setUserDtoMock: SetUserDto = {
+        email: 'some.email@mail.com',
+        accessToken: ['firstXtoken', 'secondXtoken'],
+        refreshToken: ['firstXrfXtoken', 'secondXrfXtoken'],
+      };
+      await service.setUser(key, setUserDtoMock);
+
+      expect(redisClientMock.set).toHaveBeenCalledWith(
+        key,
+        JSON.stringify(setUserDtoMock),
+      );
+    });
+  });
+
+  describe('getUser', () => {
+    it('should call redisClient.get and return parsed value', async () => {
+      const key = 'user:1';
+      const expectedValue: SetUserDto = {
+        email: 'some.email@mail.com',
+        accessToken: ['firstXtoken', 'secondXtoken'],
+        refreshToken: ['firstXrfXtoken', 'secondXrfXtoken'],
+      };
+
+      redisClientMock.get.mockResolvedValue(JSON.stringify(expectedValue));
+
+      const result = await service.getUser(key);
+
+      expect(redisClientMock.get).toHaveBeenCalledWith(key);
+      expect(result).toEqual(expectedValue);
+    });
+  });
+
+  describe('deleteUser', () => {
+    it('should call redisClient.del with correct key', async () => {
+      const key = 'user:1';
+      await service.deleteUser(key);
+
+      expect(redisClientMock.del).toHaveBeenCalledWith(key);
+    });
   });
 });
